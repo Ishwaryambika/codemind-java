2b87239e917591154dacabddaa824dad79aa21b2
Ishwaryambika <119868381+Ishwaryambika@users.noreply.github.com>
2023-08-07 19:35:11 +0530

    import java.util.Scanner; public class multi {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double n1=sc.nextInt();         double f=32+(n1*9/5);         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double n1=sc.nextInt();
+        double f=32+(n1*9/5);
+        System.out.printf("%.2f",f);
+    }
+}
