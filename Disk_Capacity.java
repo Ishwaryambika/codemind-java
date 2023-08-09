e1daab855701b13d7cf02eb36e510d4cc8bd5587
Ishwaryambika <119868381+Ishwaryambika@users.noreply.github.com>
2023-08-09 22:19:01 +0530

    import java.util.Scanner; public class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n1=sc.nextInt();         int n2=sc.nextInt();         int n3=sc.nextInt();         int n4=(n1*n2*n3*512*2);         System.out.printf("%d",n4);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int n3=sc.nextInt();
+        int n4=(n1*n2*n3*512*2);
+        System.out.printf("%d",n4);
+    }
+}
