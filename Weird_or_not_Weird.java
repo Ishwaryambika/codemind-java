26816335f169c6e53b602211111510540fa845b4
Ishwaryambika <119868381+Ishwaryambika@users.noreply.github.com>
2023-08-17 11:11:16 +0530

    import java.util.Scanner; public class Main {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%2==0)         {             if(n>2 && n<=5)             {                 System.out.print("not weird");             }             else if(n>6 && n<=20)             {                 System.out.print("weird");             }             else             {                 System.out.print("not weird");             }         }         else         {             System.out.print("weird");         }     } }

---
+++ Weird_or_not_Weird.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            if(n>2 && n<=5)
+            {
+                System.out.print("not weird");
+            }
+            else if(n>6 && n<=20)
+            {
+                System.out.print("weird");
+            }
+            else
+            {
+                System.out.print("not weird");
+            }
+        }
+        else
+        {
+            System.out.print("weird");
+        }
+    }
+}
