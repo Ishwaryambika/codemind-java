4f1fbf3806e1fe75880135dfce676e1039c965ed
Ishwaryambika <119868381+Ishwaryambika@users.noreply.github.com>
2023-11-10 14:20:46 +0530

    import java.util.Scanner; public class Main {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             float x1=sc.nextInt();             float x2=sc.nextInt();             float y1=sc.nextInt();             float y2=sc.nextInt();             if(y1/x1<y2/x2)             {                 System.out.println(-1);             }             else if(y1/x1>y2/x2)             {                 System.out.println(1);             }             else             {                 System.out.println(0);             }         }     } }

---
+++ Car_Choice.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            float x1=sc.nextInt();
+            float x2=sc.nextInt();
+            float y1=sc.nextInt();
+            float y2=sc.nextInt();
+            if(y1/x1<y2/x2)
+            {
+                System.out.println(-1);
+            }
+            else if(y1/x1>y2/x2)
+            {
+                System.out.println(1);
+            }
+            else
+            {
+                System.out.println(0);
+            }
+        }
+    }
+}
